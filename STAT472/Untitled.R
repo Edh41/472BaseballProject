24a8dbfd2cdcf74b76b92036de45c942556ba16f
Kooper Koinzan <Kooperkoinzan@gmail.com>
2024-02-28 15:47:37 -0700

    Update Untitled.R
    Start point check out lmk if problems

--- STAT472/Untitled.R
+++ STAT472/Untitled.R
@@ -1,5 +1,23 @@
-test <- "2"
+## Might need to change to have you have the csv file called on your computer
+## Load data
+BBData <- read.csv("Baseball Data - Sheet1-3.csv")
 
-test4 <-10
+## Packages That might be helpful feel free to add more
+library(ggplot2)
+library(dplyr)
+library(broom)
+library(GGally)
+library(tidyverse)
 
-n=200
+
+## Graft I like -Kooper
+# Calculate average salary for each team
+avg_salary <- BBData %>%
+  group_by(Tm) %>%
+  summarise(avg_salary = mean(Salary, na.rm = TRUE)) %>%
+  arrange(desc(avg_salary))
+ggplot(avg_salary, aes(x = reorder(Tm, avg_salary), y = avg_salary)) +
+  geom_bar(stat = "identity", fill = "skyblue") +
+  geom_hline(yintercept = 70000000, linetype = "dashed", color = "red") +  # Add a dashed red line at $70 million
+  labs(x = "Team", y = "Average Salary", title = "Average Salaries for Each Team (Highest to Lowest)") + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
