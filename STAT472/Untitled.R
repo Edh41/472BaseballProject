8e7a65500971ace20aae9405e235cf32bfa1827e
Kooper Koinzan <Kooperkoinzan@gmail.com>
2024-02-28 16:06:14 -0700

    Update Untitled.R
    add bar graft for average team salaries

--- STAT472/Untitled.R
+++ STAT472/Untitled.R
@@ -16,8 +16,11 @@ avg_salary <- BBData %>%
   group_by(Tm) %>%
   summarise(avg_salary = mean(Salary, na.rm = TRUE)) %>%
   arrange(desc(avg_salary))
+
+
 ggplot(avg_salary, aes(x = reorder(Tm, avg_salary), y = avg_salary)) +
   geom_bar(stat = "identity", fill = "skyblue") +
   geom_hline(yintercept = 70000000, linetype = "dashed", color = "red") +  # Add a dashed red line at $70 million
-  labs(x = "Team", y = "Average Salary", title = "Average Salaries for Each Team (Highest to Lowest)") + 
+  labs(x = "Team", y = "Average Salary", title = "Average Salaries for Each Team (Highest to Lowest)") +
+  theme_bw() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
